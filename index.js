298aa1305be4faba61ba7ce1c86770b178da7daf
ketankapale8 <ketan.kapale@gmail.com>
2022-08-24 19:39:03 +0530

    make it better

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/" , (req, res) =>{
 const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_DB_URL)
     .then(()=>{
-        app.listen(port , "172.20.10.4", ()=>console.log(`Server running on ${port}`))
+        app.listen(port , ()=>console.log(`Server running on ${port}`))
     })
     .catch((err)=>console.log(err))
 

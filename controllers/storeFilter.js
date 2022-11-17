b4ad84dda2ce38f317327d3ce30d506fe50056c3
ketankapale8 <ketan.kapale@gmail.com>
2022-11-18 03:11:19 +0530

    changes

--- controllers/storeFilter.js
+++ controllers/storeFilter.js
@@ -1,14 +1,12 @@
 import cartModel from "../models/cart.js";
 
 export const storeFilter = async (req ,res) =>{
-    // db.filter_test.aggregate({$project: { highmarks: { $filter: { input: "$marks", as: "marks", cond: { $gt: [ "$$marks", 65 ] } } } } } )   
     try{
-       const pos = req.body
       const storeData = await cartModel.find({$or : [{pos:"Reliance"}, {pos:'Spencer'} , {pos:'DMart'}, {pos:'BigBazaar'}]})
-    //    const storeData = await cartModel.find({pos : {$all:["Reliance", "DMart", "Spencer"]}})
-
+    //   const storeData = await cartModel.find({$or : [ {pos:'Reliance'}]})
         res.status(200).json({storeData})
     }catch(err){
         console.log(err)
     }
 }
+

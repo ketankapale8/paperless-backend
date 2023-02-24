572ec4cf008296d90872f90d8fc5177f4e8f73e0
ketankapale8 <ketan.kapale@gmail.com>
2023-02-25 01:16:27 +0530

    c

--- index.js
+++ index.js
@@ -34,22 +34,22 @@ app.use("/users", cartRouter);
 app.use("/users" , usercartRouter);
 app.use("/users/allcarts" , storeFilter);
 
-app.get('https://frail-outerwear-fish.cyclic.app/api/v1/gettokens', (req , res)=>{
-    const message = {
-        notification : {
-            title : 'Paperlesss App',
-            body : "Cart Updated"
-        },
-        token : res.tokens
-    }
-    
-    admin.messaging().send(message)
-        .then(resp=>{
-            console.log('success' , resp)
-        })
-        .catch(err=> console.log)
-    
+// app.get('https://frail-outerwear-fish.cyclic.app/api/v1/gettokens', (req , res)=>{
+// })
+
+const message = {
+    notification : {
+        title : 'Paperlesss App',
+        body : "Cart Updated"
+    },
+    token : "chNIX1kFQKuOz-VYB5qGWa:APA91bHgiVYcZaYn_vHBpAY_WMaDIcI7W6W3hO5zFaoVJOKlvVn4m_I5gm_gi_iNtj-xqfewEF01JdnGvIbr61QnRu8TpJT1w8cxTJKZ6azME3WOeYQDrIJPKwW6_gtRsP3cIPHCa8FH"
+}
+
+admin.messaging().send(message)
+    .then(resp=>{
+        console.log('success' , resp)
     })
+    .catch(err=> console.log)
     
 
 

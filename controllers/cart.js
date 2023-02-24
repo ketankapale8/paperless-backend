dcbc5623acdb157799382ee0926fbb87606b128a
ketankapale8 <ketan.kapale@gmail.com>
2023-02-25 02:10:30 +0530

    c

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,5 +1,39 @@
 import cartModel from "../models/cart.js";
 import userModal from "../models/user.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+var admin = require("firebase-admin");
+var serviceAccount = require("../controllers/paperlessapp-dbf5a-firebase-adminsdk-adyp8-6b58262710.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+
+
+require('https').get('https://frail-outerwear-fish.cyclic.app/api/v1/gettokens', (res) => {
+  res.setEncoding('utf8');
+  res.on('data', function (body) {
+      return JSON.parse(body).tokens;
+  });
+});
+const message = {
+  notification : {
+      title : 'Paperlesss App',
+      body : "Cart Updated"
+  },
+  token : "chNIX1kFQKuOz-VYB5qGWa:APA91bHgiVYcZaYn_vHBpAY_WMaDIcI7W6W3hO5zFaoVJOKlvVn4m_I5gm_gi_iNtj-xqfewEF01JdnGvIbr61QnRu8TpJT1w8cxTJKZ6azME3WOeYQDrIJPKwW6_gtRsP3cIPHCa8FH"
+}
+
+admin.messaging().send(message)
+  .then(resp=>{
+      console.log('success' , resp)
+  })
+  .catch(err=> console.log(err))
+  
+
+// import { updateUser } from "./controllers/user.js";
+
 
 
 // adding cart values
--- index.js
+++ index.js
@@ -12,12 +12,7 @@ import pkg from 'firebase-admin';
 
 const require = createRequire(import.meta.url);
 
-var admin = require("firebase-admin");
-var serviceAccount = require("../server/controllers/paperlessapp-dbf5a-firebase-adminsdk-adyp8-6b58262710.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
 
 // import { updateUser } from "./controllers/user.js";
 
@@ -38,20 +33,7 @@ app.use("/users/allcarts" , storeFilter);
 //     console.log(res)
 // })
 
-const message = {
-    notification : {
-        title : 'Paperlesss App',
-        body : "Cart Updated"
-    },
-    token : "chNIX1kFQKuOz-VYB5qGWa:APA91bHgiVYcZaYn_vHBpAY_WMaDIcI7W6W3hO5zFaoVJOKlvVn4m_I5gm_gi_iNtj-xqfewEF01JdnGvIbr61QnRu8TpJT1w8cxTJKZ6azME3WOeYQDrIJPKwW6_gtRsP3cIPHCa8FH"
-}
 
-admin.messaging().send(message)
-    .then(resp=>{
-        console.log('success' , resp)
-    })
-    .catch(err=> console.log(err))
-    
 
 
 
